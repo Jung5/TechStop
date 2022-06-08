edee0af79c5e8a6a3a067f02d327c4037b817704
JeongwookNa <najung936@hotmail.com>
2022-06-08 10:41:40 -0500

    rating system with star added

--- frontends/src/App.js
+++ frontends/src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <>
+    <Routes>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Routes>
   );
 }
 
