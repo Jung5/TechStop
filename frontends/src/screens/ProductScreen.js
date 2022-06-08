04f1e8874754f761af6af6f264c7fe00a4ee450e
JeongwookNa <najung936@hotmail.com>
2022-06-08 17:16:57 -0500

    url params prob

--- frontends/src/screens/ProductScreen.js
+++ frontends/src/screens/ProductScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
 
+const ProductScreen = ({match}) => {
+    
 
-const ProductScreen = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    return (<>
+    <Link className='btn btn-dark my-3'>To Main Page</Link>
+    <Row>
+        <Col md={6}>
+            {/* <Image src={product.image} alt={product.name} /> */}
+        </Col>
+        <Col md={3}>
+        </Col>
+    </Row>
+            </>
+    )
 };
 
-export default ProductScreen;
+export default ProductScreen; 
