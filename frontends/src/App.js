8a6a326d421f9e19d5b4ba6dea3b46b9a5fefa1b
JeongwookNa <najung936@hotmail.com>
2022-06-08 11:36:51 -0500

    replaced a tag with Link

--- frontends/src/App.js
+++ frontends/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <Routes>
+    <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact />
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Router>
         </Container>
       </main>
       <Footer />
-    </Routes>
+    </>
   );
 }
 
